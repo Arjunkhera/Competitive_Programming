9b50cfbd938fa0e8c1ff5abf2971b36cc607d7c7
arjun khera <arjunkhera@outlook.com>
2017-10-20 17:11:25 +0530

    Adding graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<list>
+
+using namespace std;
+
+class vertex;
+
+class graph
+ {
+  list<vertex> adjacency_list;
+  int nodes;
+
+  public :
+  graph(int nodes);
+  bool add_edge();
+  bool add_vertex();
+  bool remove_edge();
+  bool remove_vertex();
+ };
+
+graph::graph(int nodes=0)
+ {
+   this->nodes = nodes;
+
+   for(auto i:nodes)
+    {
+      vertex v(i);
+      adjacency_list.push_back(v);
+    }
+ }
+
+bool graph::add_edge(int src,int dest)
+ {
+   for (auto i:adjacency_list)
+    if (i.id = src)
+      {
+        vertex v(dest);
+        i.node.push_back(v);
+      }
+
+   return true;
+ }
+
+bool graph::add_vertex(int id)
+ {
+   vertex v(id);
+   adjacency_list.push_back(v);
+
+   return true;
+ }
+
+class vertex
+ {
+  list<int> node;
+  int id;
+  public :
+  vertex(int);
+ };
+
+vertex::vertex(int id)
+ {
+  this->id=id;
+ }
+
+int main()
+{
+  graph g();
+  return 0;
+}
